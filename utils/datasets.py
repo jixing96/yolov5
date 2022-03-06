457a84291b52ddb44f4df624a2f93cc8aab08b69
Jiaxu Xing <jiaxu.xing@outlook.com>
2022-03-06 12:10:57 +0100

    Add powerline yaml file

--- utils/datasets.py
+++ utils/datasets.py
@@ -379,7 +379,7 @@ class LoadImagesAndLabels(Dataset):
     # YOLOv5 train_loader/val_loader, loads images and labels for training and validation
     cache_version = 0.6  # dataset labels *.cache version
 
-    def __init__(self, path, img_size=640, batch_size=16, augment=False, hyp=None, rect=False, image_weights=False,
+    def __init__(self, path, img_size=640, batch_size=16, augment=True, hyp=None, rect=False, image_weights=False,
                  cache_images=False, single_cls=False, stride=32, pad=0.0, prefix=''):
         self.img_size = img_size
         self.augment = augment
