3c8946a736f1a237da10433749112689b753eb01
Jiaxu Xing <jiaxu.xing@outlook.com>
2022-03-29 11:59:59 +0200

    Add network inferece node

---
+++ detect_image.py
@@ -0,0 +1,92 @@
+import os
+import sys
+from pathlib import Path
+import cv2
+import numpy as np
+from numpy import imag
+import torch
+
+FILE = Path(__file__).resolve()
+ROOT = FILE.parents[0]  # YOLOv5 root directory
+if str(ROOT) not in sys.path:
+    sys.path.append(str(ROOT))  # add ROOT to PATH
+ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
+
+from models.common import DetectMultiBackend
+from utils.datasets import IMG_FORMATS, VID_FORMATS
+from utils.general import (check_file, check_img_size, check_requirements, increment_path, non_max_suppression,
+                           scale_coords)
+from utils.plots import Annotator
+from utils.torch_utils import select_device
+
+
+@torch.no_grad()
+def run(im,
+        weights=ROOT / 'runs/train/exp31/weights/best.pt',  # model.pt path(s)
+        data=ROOT / 'data/powerline_sim.yaml',  # dataset.yaml path
+        imgsz=(320, 320),  # inference size (height, width)
+        conf_thres=0.25,  # confidence threshold
+        iou_thres=0.45,  # NMS IOU threshold
+        max_det=1000,  # maximum detections per image
+        device='',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
+        save_crop=False,  # save cropped prediction boxes
+        classes=None,  # filter by class: --class 0, or --class 0 2 3
+        agnostic_nms=False,  # class-agnostic NMS
+        augment=False,  # augmented inference
+        line_thickness=3,  # bounding box thickness (pixels)
+        half=False,  # use FP16 half-precision inference
+        dnn=False,  # use OpenCV DNN for ONNX inference
+        ):
+
+    # Load model
+    device = select_device(device)
+    model = DetectMultiBackend(weights, device=device, dnn=dnn, data=data)
+    stride, names, pt, jit, onnx, engine = model.stride, model.names, model.pt, model.jit, model.onnx, model.engine
+    # Half
+    half &= (pt or jit or onnx or engine) and device.type != 'cpu'  # FP16 supported on limited backends with CUDA
+    if pt or jit:
+        model.model.half() if half else model.model.float()
+
+    bs = 1  # batch_size
+
+    # Run inference
+    model.warmup(imgsz=(1 if pt else bs, 3, *imgsz), half=half)  # warmup
+    dt, seen = [0.0, 0.0, 0.0], 0
+    im = cv2.resize(im, imgsz)
+    if im.shape[-1] == 3:
+        im = im.transpose(2, 0, 1)
+    im = torch.from_numpy(im).to(device)
+    im0 = im.cpu().numpy().copy()
+    im = im.half() if half else im.float()  # uint8 to fp16/32
+    im /= 255  # 0 - 255 to 0.0 - 1.0
+    if len(im.shape) == 3:
+        im = im[None]  # expand for batch dim
+
+    # Inference
+    pred = model(im, augment=augment)
+
+    # NMS
+    pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)
+
+    # Process predictions
+    for i, det in enumerate(pred):  # per image
+        seen += 1
+        gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+        imc = im0.copy() if save_crop else im0  # for save_crop
+        annotator = Annotator(im0, line_width=line_thickness, example=str(names))
+        if len(det):
+            # Rescale boxes from img_size to im0 size
+            det[:, :4] = scale_coords(im.shape[2:], det[:, :4], im0.shape).round()
+
+    return im0.transpose(1, 2, 0)
+
+
+def main():
+    image = np.zeros([480, 720, 3])
+    check_requirements(exclude=('tensorboard', 'thop'))
+    image = run(im=image)
+    print(image.shape)
+
+
+if __name__ == "__main__":
+    main()
