ee03418bb8911fa72f0e3bdd2c82b297b0a93fad
Jiaxu Xing <jiaxu.xing@outlook.com>
2022-04-01 20:25:03 +0200

    Vision closed-loop worked

--- detect_image.py
+++ detect_image.py
@@ -5,6 +5,7 @@ import cv2
 import numpy as np
 from numpy import imag
 import torch
+import time
 
 FILE = Path(__file__).resolve()
 ROOT = FILE.parents[0]  # YOLOv5 root directory
@@ -19,26 +20,17 @@ from utils.general import (check_file, check_img_size, check_requirements, incre
 from utils.plots import Annotator, colors
 from utils.torch_utils import select_device
 
-
-@torch.no_grad()
-def run(im,
-        weights=ROOT / 'runs/train/exp31/weights/best.pt',  # model.pt path(s)
+def load_model(weights=ROOT / 'runs/train/exp31/weights/best.pt',  # model.pt path(s)
         data=ROOT / 'data/powerline_sim.yaml',  # dataset.yaml path
-        imgsz=(320, 320),  # inference size (height, width)
-        conf_thres=0.25,  # confidence threshold
-        iou_thres=0.45,  # NMS IOU threshold
-        max_det=1000,  # maximum detections per image
-        device='',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
-        save_crop=False,  # save cropped prediction boxes
-        classes=None,  # filter by class: --class 0, or --class 0 2 3
-        agnostic_nms=False,  # class-agnostic NMS
-        augment=False,  # augmented inference
-        line_thickness=3,  # bounding box thickness (pixels)
+        device = '',
+        dnn=False,# use OpenCV DNN for ONNX inference
         half=False,  # use FP16 half-precision inference
-        dnn=False,  # use OpenCV DNN for ONNX inference
+        imgsz=(320, 320)
         ):
-
     # Load model
+    start_time = time.time()
+    
+
     device = select_device(device)
     model = DetectMultiBackend(weights, device=device, dnn=dnn, data=data)
     stride, names, pt, jit, onnx, engine = model.stride, model.names, model.pt, model.jit, model.onnx, model.engine
@@ -48,49 +40,67 @@ def run(im,
         model.model.half() if half else model.model.float()
 
     bs = 1  # batch_size
+    model.warmup(imgsz=(1 if pt else bs, 3, *imgsz), half=half)  # warmup
+    return model, pt, bs, device, names
+
+
+@torch.no_grad()
+def run(im,
+        model,
+        pt, 
+        bs,
+        device,
+        names,
+        imgsz=(320, 320),  # inference size (height, width)
+        conf_thres=0.25,  # confidence threshold
+        iou_thres=0.45,  # NMS IOU threshold
+        max_det=1000,  # maximum detections per image
+        save_crop=False,  # save cropped prediction boxes
+        classes=None,  # filter by class: --class 0, or --class 0 2 3
+        agnostic_nms=False,  # class-agnostic NMS
+        augment=False,  # augmented inference
+        line_thickness=3,  # bounding box thickness (pixels)
+        half=False  # use FP16 half-precision inference
+        ):
 
     # Run inference
-    model.warmup(imgsz=(1 if pt else bs, 3, *imgsz), half=half)  # warmup
     dt, seen = [0.0, 0.0, 0.0], 0
     im = cv2.resize(im, imgsz)
     if im.shape[-1] == 3:
         im = im.transpose(2, 0, 1)
+    start_time = time.time()
     im = torch.from_numpy(im).to(device)
     im0 = im.cpu().numpy().copy().transpose(1, 2, 0)
     im = im.half() if half else im.float()  # uint8 to fp16/32
     im /= 255  # 0 - 255 to 0.0 - 1.0
     if len(im.shape) == 3:
         im = im[None]  # expand for batch dim
-
     # Inference
     pred = model(im, augment=augment)
 
+
     # NMS
     pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)
-
     # Process predictions
     for i, det in enumerate(pred):  # per image
         seen += 1
         gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
         imc = im0.copy() if save_crop else im0  # for save_crop
-        annotator = Annotator(np.ascontiguousarray(im0), line_width=line_thickness, example=str(names))
+        # annotator = Annotator(np.ascontiguousarray(im0), line_width=line_thickness, example=str(names))
         if len(det):
             # Rescale boxes from img_size to im0 size
             det[:, :4] = scale_coords(im.shape[2:], det[:, :4], im0.shape).round()
 
             # Print results
-            for c in det[:, -1].unique():
-                n = (det[:, -1] == c).sum()  # detections per class
+            # for c in det[:, -1].unique():
+            #     n = (det[:, -1] == c).sum()  # detections per class
 
             # Write results
-            for *xyxy, conf, cls in reversed(det):
-                if conf >= 0.0:
-                        c = int(cls)  # integer class
-                        label = f'{names[c]} {conf:.2f}'
-                        annotator.box_label(xyxy, label, color=colors(c, True))
-
-            # Stream results
-            im0 = annotator.result()
+            # for *xyxy, conf, cls in reversed(det):
+            #     if conf >= 0.0:
+            #             c = int(cls)  # integer class
+            #             label = f'{names[c]} {conf:.2f}'
+                        # annotator.box_label(xyxy, label, color=colors(c, True))
 
     return det.cpu().numpy(), im0
 
