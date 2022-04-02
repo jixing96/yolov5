941f30353ab7a15ae64406c0e48076218840e71b
Jiaxu Xing <jiaxu.xing@outlook.com>
2022-04-02 17:21:33 +0200

    Depth closed loop worked

--- detect_image.py
+++ detect_image.py
@@ -28,9 +28,6 @@ def load_model(weights=ROOT / 'runs/train/exp31/weights/best.pt',  # model.pt pa
         imgsz=(320, 320)
         ):
     # Load model
-    start_time = time.time()
-    
-
     device = select_device(device)
     model = DetectMultiBackend(weights, device=device, dnn=dnn, data=data)
     stride, names, pt, jit, onnx, engine = model.stride, model.names, model.pt, model.jit, model.onnx, model.engine
@@ -55,7 +52,6 @@ def run(im,
         conf_thres=0.25,  # confidence threshold
         iou_thres=0.45,  # NMS IOU threshold
         max_det=1000,  # maximum detections per image
-        save_crop=False,  # save cropped prediction boxes
         classes=None,  # filter by class: --class 0, or --class 0 2 3
         agnostic_nms=False,  # class-agnostic NMS
         augment=False,  # augmented inference
@@ -68,7 +64,6 @@ def run(im,
     im = cv2.resize(im, imgsz)
     if im.shape[-1] == 3:
         im = im.transpose(2, 0, 1)
-    start_time = time.time()
     im = torch.from_numpy(im).to(device)
     im0 = im.cpu().numpy().copy().transpose(1, 2, 0)
     im = im.half() if half else im.float()  # uint8 to fp16/32
@@ -84,24 +79,20 @@ def run(im,
     # Process predictions
     for i, det in enumerate(pred):  # per image
         seen += 1
-        gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
-        imc = im0.copy() if save_crop else im0  # for save_crop
-        # annotator = Annotator(np.ascontiguousarray(im0), line_width=line_thickness, example=str(names))
+        annotator = Annotator(np.ascontiguousarray(im0), line_width=line_thickness, example=str(names))
+                       
         if len(det):
             # Rescale boxes from img_size to im0 size
             det[:, :4] = scale_coords(im.shape[2:], det[:, :4], im0.shape).round()
 
-            # Print results
-            # for c in det[:, -1].unique():
-            #     n = (det[:, -1] == c).sum()  # detections per class
-
             # Write results
-            # for *xyxy, conf, cls in reversed(det):
-            #     if conf >= 0.0:
-            #             c = int(cls)  # integer class
-            #             label = f'{names[c]} {conf:.2f}'
-                        # annotator.box_label(xyxy, label, color=colors(c, True))
-
+            for *xyxy, conf, cls in reversed(det):
+                if conf >= 0.0:
+                    c = int(cls)  # integer class
+                    label = f'{names[c]} {conf:.2f}'
+                    annotator.box_label(xyxy, label, color=colors(c, True))
+                    im0 = annotator.result()
+                    
     return det.cpu().numpy(), im0
 
 
